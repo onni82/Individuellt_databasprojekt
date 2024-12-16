53f1f2329ace6e54a684e242c8cd57b4003d3efe
onni82 <onni.bucht@me.com>
2024-12-16 13:12:00 +0100

    Add project files.

---
+++ Individuellt_databasprojekt/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Individuellt_databasprojekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

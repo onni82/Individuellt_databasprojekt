e734f49080ec52889c31dde02b2116d3a1251be1
onni82 <onni.bucht@me.com>
2024-12-22 00:34:24 +0100

    Added comments

--- Individuellt_databasprojekt/Program.cs
+++ Individuellt_databasprojekt/Program.cs
@@ -4,8 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Menu.Run();
-        }
+            Menu.Run(); // Runs a menu based on Entity Framework (ORM)
+			//SQLMenu.Run(); // Runs a menu based on ADO.NET instead of Entity Framework (ORM)
+		}
     }
 }
 // Scaffold-DbContext "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=School;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -ContextDir Data -Context SchoolContext
